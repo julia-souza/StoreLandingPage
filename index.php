b605317f9cdcacdd390a85a46a760a2e1a6ca39b
Júlia Souza <juliadsouzaborges@gmail.com>
2022-03-25 18:42:12 -0300

    criação desing inicial header

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
+
+    <title>Templates</title>
+
+</head>
+
+<body>
+    
+    <section>
+        <header>
+            <a href="#" > <img src="img/logo.png" alt="logo" class="logo"> </a>
+            <ul>
+                <li><a href="#">HOME</a></li> 
+                <li><a href="#">TEMPLATES</a></li>
+                <li><a href="#">CONTATO</a></li>
+             </ul>
+        </header>
+
+
+    </section>
+
+
+</body>
+
+</html>
